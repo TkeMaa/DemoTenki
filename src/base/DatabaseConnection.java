e314778f2f7c8b02995941d0e2b670ffa25a260e
Matija <matijaperos28@gmail.com>
2024-09-09 20:38:50 +0200

    Promenjena baza

--- src/base/DatabaseConnection.java
+++ src/base/DatabaseConnection.java
@@ -7,11 +7,11 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
 	private static final String host = "sql7.freesqldatabase.com";
-	private static final String dbName = "sql7726250";
+	private static final String dbName = "sql7730432";
 	
 	private static final String url = "jdbc:mysql://" + host + ":3306/" + dbName;
-	private static final String user = "sql7726250";
-	private static final String password = "NhmlgxDYcx";
+	private static final String user = "sql7730432";
+	private static final String password = "bzKrAFEIsh";
 	
 	public static Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(url, user, password);
